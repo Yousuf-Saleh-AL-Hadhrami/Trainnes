1a3dda6a715b98801610ce09051575c9cd14e30f
Yousuf AL Hadhrami <izkiboy@gmail.com>
2025-06-16 08:25:52 +0400

    The first project structure

---
+++ project3/js/main.js
@@ -0,0 +1,17 @@
+
+
+function hello(x , y){
+
+    return x + y;
+}
+
+console.log(hello(5 , 9));
+
+
+
+let div = document.getElementsByClassName("one")[0];
+
+div.addEventListener("click", function(){
+
+    div.style.backgroundColor="beige";
+})
