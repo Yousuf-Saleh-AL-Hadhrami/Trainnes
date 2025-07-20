bd2e064f7bc6f4e894a209bd81ac7f32e6844792
Yousuf Saleh Issa AL Hadhrami <izkiboy@gmail.com>
2025-07-20 11:14:01 +0400

    added oop folder

---
+++ php_course/oop/Person.php
@@ -0,0 +1,151 @@
+<?php 
+declare(strict_types=1);
+/* 
+$name = "Yousuf";
+$address = "Izki";
+$job = "Proigrammer";
+
+function setInfo($n , $address , $job){
+
+    return $n . " " . $address . " " . $job;
+}
+
+
+// Overrding 
+$name = "Ali";
+$address = "Izki";
+$job = "Proigrammer";
+
+echo $name;
+
+
+echo "<br>";
+
+echo setInfo("Yousuf", "Izki", "Programmer");
+
+*/
+
+
+function pre($obj){
+
+    echo "<pre>";
+       var_dump($obj);
+    echo "</pre>";
+
+
+}
+
+
+class Person {
+
+    public $name;
+    public $address;
+    public $job;
+    public $bsalary = 500;
+    public $totalSalary = 0;
+    protected $password;
+    public static $counter = 0;
+
+   public const GENDER = 18;
+
+    public function __construct($n , $add , $j) {
+        $this->name = $n;
+        $this->address = $add;
+        $this->job = $j;
+
+        self::$counter++;
+    }
+
+    public function setName($n) {
+        $this->name = $n;
+        return $this;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAddress($ad) {
+        $this->address = $ad;
+        return $this;
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function setJob(string $j) {
+        $this->job = $j;
+        return $this;
+    }
+
+    public function getJob() {
+        return $this->job;
+    }
+
+    public function getSalary() {
+        return $this->bsalary;
+    }
+
+    public function addBouns($per) {
+        // Add bonus to the base salary and also keep track of total salary
+        $this->bsalary += $this->bsalary * $per;
+        $this->totalSalary = $this->bsalary;
+        return $this;
+    }
+
+    public function deductBouns($per) {
+        // Deduct bonus from the salary and update total salary
+        $this->bsalary -= $this->bsalary * $per;
+        $this->totalSalary = $this->bsalary;
+        return $this;
+    }
+
+    public function setPassword($pw){
+        $this->password =  password_hash($pw , PASSWORD_DEFAULT);
+
+        return $this;
+    }
+
+    public function getPassword(){
+
+        return $this->password;
+    }
+
+    public function printAll() {
+        return "Name : " . $this->getName() . "<br>" .
+               "Address : " . $this->getAddress() . " <br>" .
+               "Job : " . $this->getJob() . " <br>" .
+               "Salary : " . $this->getSalary() . "<br> " .
+               "Password : " . $this->getPassword() . "<br> " .
+               "Number of objects from this class  : " . Person::$counter . "<br> " .
+               "Total Salary : " . $this->totalSalary . "<br>";
+    }
+}
+
+// Usage
+$p1 = new Person("Yousuf", "Izki", "Programmer");
+$p2 = new Person("Yousuf1", "Izki", "Programmer");
+$p3 = new Person("Yousuf2", "Izki", "Programmer");
+
+
+echo $p1->setName("Yousuf")
+         ->setJob("Programmer")
+         ->addBouns(0.05)  // Adding 5% bonus
+         ->setPassword("123456")
+         ->printAll();      // Printing details
+
+         echo $p2->setName("Yousuf")
+         ->setJob("Programmer")
+         ->addBouns(0.05)  // Adding 5% bonus
+         ->setPassword("123456")
+         ->printAll();      // Printing details
+
+
+         echo $p3->setName("Yousuf")
+         ->setJob("Programmer")
+         ->addBouns(0.05)  // Adding 5% bonus
+         ->setPassword("123456")
+         ->printAll();      // Printing details
+
+
